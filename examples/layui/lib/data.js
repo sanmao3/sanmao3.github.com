ab3319b1ee6ca8b30691c8057a5077b3b7fcfb04
Sihua Lee <lisihua0916@gmail.com>
2023-01-28 14:30:24 +0800

    update examples

---
+++ examples/layui/lib/data.js
@@ -0,0 +1,57 @@
+let excelData = [{
+    "id": 1,
+    "username": "wang",
+    "age": 10,
+    "sex": "男",
+    "score": 100,
+    "classify": "程序猿"
+  },
+  {
+    "id": 1,
+    "username": "wang",
+    "age": 10,
+    "sex": "男",
+    "score": 100,
+    "classify": "程序猿"
+  },
+  {
+    "id": 1,
+    "username": "wang",
+    "age": 10,
+    "sex": "男",
+    "score": 100,
+    "classify": "程序猿"
+  },
+  {
+    "id": 1,
+    "username": "wang",
+    "age": 10,
+    "sex": "男",
+    "score": 100,
+    "classify": "程序猿"
+  },
+  {
+    "id": 1,
+    "username": "wang",
+    "age": 10,
+    "sex": "男",
+    "score": 100,
+    "classify": "程序猿"
+  },
+  {
+    "id": 1,
+    "username": "wang",
+    "age": 10,
+    "sex": "男",
+    "score": 100,
+    "classify": "程序猿"
+  },
+  {
+    "id": 1,
+    "username": "wang",
+    "age": 10,
+    "sex": "男",
+    "score": 100,
+    "classify": "程序猿"
+  }
+]
